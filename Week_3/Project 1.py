fa01d7e18d2bdc15f17ef80b3621cbdccd47e24b
Jay6896 <jamilsalami08@gmail.com>
2025-03-29 13:53:01 +0100

    ''

---
+++ Week_3/Project 1.py
@@ -0,0 +1,40 @@
+girlNames = ["Evelyn", "Jessica", "Somto", "Edith", "Liza", "Madonna", "Waje", "Tola", "Aisha", "Latifa"]
+girlAge = [17, 16, 17, 18, 16, 18, 17, 20, 19, 17]
+girlHeight = [5.5, 6.0, 5.4, 5.9, 5.6, 5.5, 5.6, 6.0, 5.7, 5.5]
+girlScores = [80, 85, 70, 60, 76, 66, 87, 95, 50, 49]
+
+boyNames = ["Chinedu", "Liam", "Wale", "Gbenga", "Abiola", "Kola", "Kunle", "George", "Thomas", "Wesley"]
+boyAge = [19, 16, 18, 17, 20, 19, 16, 18, 17, 19]
+boyHeight = [5.7, 5.9, 5.8, 6.1, 5.9, 5.5, 6.1, 5.4, 5.8, 5.7]
+boyScores = [74, 87, 75, 68, 66, 78, 87, 98, 54, 60]
+
+print("In a Class of 20 students consisting of 10 boys and 10 girls, \nthese are the names, ages, heights and scores of the students")
+
+print("\nGirls:")
+print("|Names   |Age |Height  |Scores |")
+print("--------------------------------")
+print(f"|{girlNames[0]}  |{girlAge[0]}  |{girlHeight[0]}     |{girlScores[0]}     |")
+print(f"|{girlNames[1]} |{girlAge[1]}  |{girlHeight[1]}     |{girlScores[1]}     |")
+print(f"|{girlNames[2]}   |{girlAge[2]}  |{girlHeight[2]}     |{girlScores[2]}     |")
+print(f"|{girlNames[3]}   |{girlAge[3]}  |{girlHeight[3]}     |{girlScores[3]}     |")
+print(f"|{girlNames[4]}    |{girlAge[4]}  |{girlHeight[4]}     |{girlScores[4]}     |")
+print(f"|{girlNames[5]} |{girlAge[5]}  |{girlHeight[5]}     |{girlScores[5]}     |")
+print(f"|{girlNames[6]}    |{girlAge[6]}  |{girlHeight[6]}     |{girlScores[6]}     |")
+print(f"|{girlNames[7]}    |{girlAge[7]}  |{girlHeight[7]}     |{girlScores[7]}     |")
+print(f"|{girlNames[8]}   |{girlAge[8]}  |{girlHeight[8]}     |{girlScores[8]}     |")
+print(f"|{girlNames[9]}  |{girlAge[9]}  |{girlHeight[9]}     |{girlScores[9]}     |")
+
+print("\nBoys:")
+print("|Names   |Age |Height  |Scores |")
+print("--------------------------------")
+print(f"|{boyNames[0]} |{boyAge[0]}  |{boyHeight[0]}     |{boyScores[0]}     |")
+print(f"|{boyNames[1]}    |{boyAge[1]}  |{boyHeight[1]}     |{boyScores[1]}     |")
+print(f"|{boyNames[2]}    |{boyAge[2]}  |{boyHeight[2]}     |{boyScores[2]}     |")
+print(f"|{boyNames[3]}  |{boyAge[3]}  |{boyHeight[3]}     |{boyScores[3]}     |")
+print(f"|{boyNames[4]}  |{boyAge[4]}  |{boyHeight[4]}     |{boyScores[4]}     |")
+print(f"|{boyNames[5]}    |{boyAge[5]}  |{boyHeight[5]}     |{boyScores[5]}     |")
+print(f"|{boyNames[6]}   |{boyAge[6]}  |{boyHeight[6]}     |{boyScores[6]}     |")
+print(f"|{boyNames[7]}  |{boyAge[7]}  |{boyHeight[7]}     |{boyScores[7]}     |")
+print(f"|{boyNames[8]}  |{boyAge[8]}  |{boyHeight[8]}     |{boyScores[8]}     |")
+print(f"|{boyNames[9]}  |{boyAge[9]}  |{boyHeight[9]}     |{boyScores[9]}     |")
+
